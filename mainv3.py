1e5fa8f2bf05de7b844497730452c268e2f950fc
bthoo3h <htbooth@uark.edu>
2021-11-15 13:44:45 -0600

    Testing to main

--- mainv3.py
+++ mainv3.py
@@ -1,22 +1,18 @@
-import tkinter as tk
+"""Instead of using basic tkinter, ttkthemes is used. It just looks prettier"""
+from ttkthemes import ThemedTk, THEMES, ThemedStyle
 from tkinter import ttk
 from tkinter import font as tkfont
-from ttkthemes import ThemedTk
-# TODO: Add this import statement in the README: https://ttkthemes.readthedocs.io/en/latest/installation.html
-
-import dbconnect
-import json
-import time
 
 
-class Container(tk.Tk):
-    """ Main Class for the file
-        Sets up "global variables" for the file
+class Container(ThemedTk):
 
-    """
-
+    # Main Class for the file
+    # Sets up "global variables" for the file
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(self, *args, **kwargs)
+
+        ThemedTk.__init__(self, fonts=True, themebg=True,
+                          toplevel=True, *args, **kwargs)
+        self.set_theme("equilux")
 
         # Font setup
         # TODO: Setup colors
@@ -37,23 +33,19 @@ class Container(tk.Tk):
         self.PLAYERACTION_LENGTH = 60
 
         # Window title
-        tk.Tk.wm_title(self, "PHOTON")
-
-        # TODO: Does this work?
-        tk.Tk.ThemedTk(theme="equilux")
+        ThemedTk.wm_title(self, "PHOTON")
 
         # Basic grid set up
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
     def show_frame(self, container):
-        """Pull up the correct frame, setting Container as the container"""
+        # Pull up the correct frame, setting Container as the container
         # TODO: Make this initialize the frame rather than just raise it
         frame = self.frames[container]
         frame.tkraise()
 
 
-
 if __name__ == "__main__":
-    root = Container
+    root = Container()
     root.mainloop()
